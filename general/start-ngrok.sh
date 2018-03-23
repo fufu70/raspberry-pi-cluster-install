4a7e228c246620a2c723ae0971f5c915a15f43d3
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-03-23 05:27:16 -0400

    Added a path to the start-ngrok script

--- general/start-ngrok.sh
+++ general/start-ngrok.sh
@@ -3,5 +3,5 @@
 # Starts the ngrok server
 nohup ./ngrok tcp 22 &
 sleep 30 # Wait for a connection to be made
-RESULTS=$(python get-ngrok-tunnel.py)
+RESULTS=$(python /home/pi/raspberry-pi-cluster-install/general/get-ngrok-tunnel.py)
 echo $RESULTS
