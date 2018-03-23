48cd203a250ac1f937c8271349cb7985245efcc2
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-03-22 23:50:08 -0400

    Update ngrok.sh

--- general/ngrok.sh
+++ general/ngrok.sh
@@ -15,6 +15,6 @@ gvm use go1.4
 export GOROOT_BOOTSTRAP=$GOROOT
 
 # Install all of the necessary ngrok tools to allow remote ssh capabilities
-git clone https://github.com/inconschreveable/ngrok.git
+git clone git://github.com/inconshreveable/ngrok.git
 cd ngrok && make
 bin/ngrok -proto=tcp 22
