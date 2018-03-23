f1a295e4df8fd1e377f445e97c0b18f83239d77c
Christian Micklisch <christian.micklisch@successwithsos.com>
2018-03-23 00:57:44 -0400

    Update ngrok.sh

--- general/ngrok.sh
+++ general/ngrok.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-# Based off of Nicola Malizia's Ngrok installation tutorial
-#
-# @link https://medium.com/@unnikked/how-to-compile-ngrok-on-a-raspberry-pi-d5a78ce4b15c
 
-# Install Go
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-sudo apt-get -y --force-yes install bison
-sudo echo "[[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm >> ~/.bashrc
-source ~/.bashrc
-gvm install go1.4
+# Download ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip 
 
-# Use Go v1.4
-gvm use go1.4
-export GOROOT_BOOTSTRAP=$GOROOT
-
-# Install all of the necessary ngrok tools to allow remote ssh capabilities
-git clone git://github.com/inconshreveable/ngrok.git
-cd ngrok && make
-bin/ngrok -proto=tcp 22
+# Add in personal Authtoken (Will remove one day)
+./ngrok authtoken S9LTo8e9E2gTBGGTBkLV_5ApQKZeTgASVfRdZvjJBT
