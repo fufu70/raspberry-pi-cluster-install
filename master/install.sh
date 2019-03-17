7061d8ca00320de5bfa530236054c348b9f60e30
Christian Micklisch <christian.micklisch@successwithsos.com>
2019-03-17 19:51:00 -0400

    Update install.sh

--- master/install.sh
+++ master/install.sh
@@ -9,6 +9,6 @@ echo
 
 SLAVE_IPS=`cat $FILE`
 for SLAVE_IP in $SLAVE_IPS; do
-	sshpass $SLAVE_PASS ssh "${SLAVE_USER}@${SLAVE_IP}" "git clone https://github.com/fufu70/raspberry-pi-cluster-install"
-	sshpass $SLAVE_PASS ssh "${SLAVE_USER}@${SLAVE_IP}" "echo '${SLAVE_PASS}' | sudo bash ./raspberry-pi-cluster-install/general/install.sh"
+	sshpass -p $SLAVE_PASS ssh "${SLAVE_USER}@${SLAVE_IP}" "git clone https://github.com/fufu70/raspberry-pi-cluster-install"
+	sshpass -p $SLAVE_PASS ssh "${SLAVE_USER}@${SLAVE_IP}" "echo '${SLAVE_PASS}' | sudo bash ./raspberry-pi-cluster-install/general/install.sh"
 done
