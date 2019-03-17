d321c9580f5e76619aadc74dd4efe2fe1ad462a0
Christian Micklisch <christian.micklisch@successwithsos.com>
2019-03-17 19:51:17 -0400

    Update send.sh

--- master/send.sh
+++ master/send.sh
@@ -13,7 +13,7 @@ echo
 
 SLAVE_IPS=`cat $FILE`
 for SLAVE_IP in $SLAVE_IPS; do
-	sshpass $SLAVE_PASS sftp -oBatchMode=no -b - "${SLAVE_USER}@${SLAVE_IP}" << !
+	sshpass -p $SLAVE_PASS sftp -oBatchMode=no -b - "${SLAVE_USER}@${SLAVE_IP}" << !
 		put "${FILE_NAME}"
 		bye
 	!
